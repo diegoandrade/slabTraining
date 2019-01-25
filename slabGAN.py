838df6002e8c2ca0a7f8b0f64e87ad4dca537bbe
Diego Andrade <diegoandrade@gmail.com>
2019-01-24 21:09:28 -0500

    Added termgraph
    This creates a graphic on the console of the usage of the system more info at
    termgraph.py

--- slabGAN.py
+++ slabGAN.py
@@ -289,6 +289,7 @@ def train():
             text_file.write("%d \t\t%f \t\t%f \t\t%s \t\t%s \n" % (i, dLoss, gLoss, (time.time() - epoch_time),(time.time() - start_time)))
             text_file.close()
 
+        termGraphCall(i,EPOCH)
 
 
 
@@ -352,9 +353,11 @@ def utilClahe():
 ##Used to equalize the images currently used
     equ=None
     clahe = cv2.createCLAHE(clipLimit=10.0, tileGridSize=(8,8))
-
+    #maxIter = 500
     for k in range(count-1):
         print(k)
+        time.sleep(0.5)
+        termGraphCall(k,maxIter)
         img = cv2.imread(images[k+1], cv2.IMREAD_GRAYSCALE)
         equ=cv2.normalize(img, equ, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
         cv2.imwrite((newDataClahe + '/%d_n.png') % k, equ)
@@ -362,6 +365,15 @@ def utilClahe():
         cv2.imwrite((newDataClahe + '/%d_e.png') % k, hist)
         cl = clahe.apply(img)
         cv2.imwrite((newDataClahe + '/%d_c.png') % k, cl)
+
+
+def termGraphCall(currentIter,maxIter):
+    text_file = open("iterationMeter.dat","r+")
+    text_file.write("# This file manages the time of execution termgraph\n")
+    text_file.write("@ Current Iteration,Total Iterations\n")
+    text_file.write("2018\t\t%d \t\t%d \n" % (currentIter,maxIter))
+    text_file.close()
+
 
 if __name__ == "__main__":
     train()
